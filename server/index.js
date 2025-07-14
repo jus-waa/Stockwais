009a6f722b00f06dae7e65f450c5c1b49e913af0
josh <joshlagrimas1808@gmail.com>
2025-07-14 23:49:29 +0800

    feat: added route and controller, sign up is ready

--- server/index.js
+++ server/index.js
@@ -1,17 +1,22 @@
 import express from "express";
 const app = express();
+const port = process.env.PORT || 5000;
 
 import morgan from "morgan";
 import helmet from "helmet";
 import dotenv from "dotenv";
 import cors from "cors";
 
-dotenv.config();
-const port = process.env.PORT || 5000;
+import authRoutes from "./routes/auth.route.js";
 
-app.use('/', authRoutes => {
-    res.send("Hello world");
-});
+dotenv.config();
+
+app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(morgan("dev"));
+
+app.use('/api/v1/auth', authRoutes);
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
